4c3643462533108ecb16db0ce544c643c51496a9
justinpawela <justinpawela@users.noreply.github.com>
2015-09-27 13:14:45 -0500

    Cleaned up LogKit file
    - Removed a few unneeded functions and extensions.
    - Reorganized the file.
    - Removed some unnecessary OS version checking.

--- Source/LogKit.swift
+++ Source/LogKit.swift
@@ -51,76 +51,8 @@ internal let LK_DEFAULT_LOG_DIRECTORY: NSURL? = {
 }()
 
 
-internal extension NSCalendar {
-
-    internal func isDateSameAsToday(date: NSDate) -> Bool {
-        if #available(OSX 10.9, OSXApplicationExtension 10.9,  iOS 8.0, iOSApplicationExtension 8.0, watchOS 2.0, *) {
-            return self.isDateInToday(date)
-        } else {
-            let today = NSDate()
-            let todayDay = self.ordinalityOfUnit(.Day, inUnit: .Year, forDate: today)
-            let todayYear = self.ordinalityOfUnit(.Year, inUnit: .Era, forDate: today)
-            let dateDay = self.ordinalityOfUnit(.Day, inUnit: .Year, forDate: date)
-            let dateYear = self.ordinalityOfUnit(.Year, inUnit: .Era, forDate: date)
-            return todayYear == dateYear && todayDay == dateDay
-        }
-    }
-
-}
-
-
-internal extension String {
-    /// Returns a dispatch_data_t object containing a representation of the String encoded using a given encoding.
-    func dispatchDataUsingEncoding(encoding: NSStringEncoding) -> dispatch_data_t? {
-        if let nData = self.dataUsingEncoding(encoding), dData = dispatch_data_create(nData.bytes, nData.length, nil, nil) {
-            return dData
-        } else {
-            return nil
-        }
-    }
-
-}
-
-
-internal extension NSFileManager {
-
-    internal func ensureFileAtURL(URL: NSURL, withIntermediateDirectories createDirs: Bool) -> Bool {
-        if let dirURL = URL.URLByDeletingLastPathComponent, path = URL.path {
-            do {
-                let manager = NSFileManager.defaultManager()
-                try manager.createDirectoryAtURL(dirURL, withIntermediateDirectories: createDirs, attributes: nil)
-                return manager.fileExistsAtPath(path) ? true : manager.createFileAtPath(path, contents: nil, attributes: nil)
-            } catch {
-                assertionFailure("File system error (maybe access denied) at path: '\(path)'")
-            }
-        }
-        assertionFailure("Invalid file path '\(URL.absoluteString)'")
-        return false
-    }
-
-}
-
-
-internal func dispatchRepeatingTimer(
-    delay delay: NSTimeInterval,
-    interval: NSTimeInterval,
-    tolerance: NSTimeInterval,
-    handler: () -> Void
-) -> dispatch_source_t {
-    let DOUBLE_NANO_PER_SEC = Double(NSEC_PER_SEC)
-    let timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, LK_LOGKIT_QUEUE)
-    dispatch_source_set_event_handler(timer, handler)
-    dispatch_source_set_timer(
-        timer,
-        dispatch_time(DISPATCH_TIME_NOW, Int64(delay * DOUBLE_NANO_PER_SEC)),
-        UInt64(interval * DOUBLE_NANO_PER_SEC),
-        UInt64(tolerance * DOUBLE_NANO_PER_SEC)
-    )
-    return timer
-}
-
-
 internal let LK_BUNDLE_ID: String = NSBundle.mainBundle().bundleIdentifier ?? ""
+
 
 internal let LK_DEVICE_MODEL: String = {
     var len: size_t = 0
@@ -133,26 +65,23 @@ internal let LK_DEVICE_MODEL: String = {
     return ""
 }()
 
+
 internal let LK_DEVICE_OS: (decription: String, majorVersion: Int, minorVersion: Int, patchVersion: Int, buildVersion: String) = {
-    var hold: (String, Int, Int, Int, String)?
-    dispatch_sync(LK_LOGKIT_QUEUE, {
-        let systemVersion = NSDictionary(contentsOfFile: "/System/Library/CoreServices/SystemVersion.plist")
-        let build = systemVersion?["ProductBuildVersion"] as? String ?? ""
-        let info = NSProcessInfo.processInfo()
-        let description = info.operatingSystemVersionString
-        if #available(OSX 10.10, OSXApplicationExtension 10.10,  iOS 8.0, iOSApplicationExtension 8.0, watchOS 2.0, *) {
-            let version = info.operatingSystemVersion
-            hold = (description, version.majorVersion, version.minorVersion, version.patchVersion, build)
-        } else {
-            let version = systemVersion?["ProductVersion"] as? String
-            let parts = version?.characters.split(".") ?? []
-            let major = parts.count > 0 ? Int(String(parts[0])) ?? -1 : -1
-            let minor = parts.count > 1 ? Int(String(parts[1])) ?? -1 : -1
-            let patch = parts.count > 2 ? Int(String(parts[2])) ?? -1 : -1
-            hold = (description, major, minor, patch, build)
-        }
-    })
-    return hold!
+    let systemVersion = NSDictionary(contentsOfFile: "/System/Library/CoreServices/SystemVersion.plist")
+    let build = systemVersion?["ProductBuildVersion"] as? String ?? ""
+    let info = NSProcessInfo.processInfo()
+    let description = info.operatingSystemVersionString
+    if #available(OSX 10.10, OSXApplicationExtension 10.10,  iOS 8.0, iOSApplicationExtension 8.0, watchOS 2.0, *) {
+        let version = info.operatingSystemVersion
+        return (description, version.majorVersion, version.minorVersion, version.patchVersion, build)
+    } else {
+        let version = systemVersion?["ProductVersion"] as? String
+        let parts = version?.characters.split(".") ?? []
+        let major = parts.count > 0 ? Int(String(parts[0])) ?? -1 : -1
+        let minor = parts.count > 1 ? Int(String(parts[1])) ?? -1 : -1
+        let patch = parts.count > 2 ? Int(String(parts[2])) ?? -1 : -1
+        return (description, major, minor, patch, build)
+    }
 }()
 
 
@@ -174,3 +103,40 @@ internal let LK_DEVICE_IDS: (vendor: String, advertising: String) = {
     return ("", "")
 #endif
 }()
+
+
+internal extension NSFileManager {
+
+    internal func ensureFileAtURL(URL: NSURL, withIntermediateDirectories createDirs: Bool) -> Bool {
+        if let dirURL = URL.URLByDeletingLastPathComponent, path = URL.path {
+            do {
+                let manager = NSFileManager.defaultManager()
+                try manager.createDirectoryAtURL(dirURL, withIntermediateDirectories: createDirs, attributes: nil)
+                return manager.fileExistsAtPath(path) ? true : manager.createFileAtPath(path, contents: nil, attributes: nil)
+            } catch {
+                assertionFailure("File system error (maybe access denied) at path: '\(path)'")
+            }
+        }
+        assertionFailure("Invalid file path '\(URL.absoluteString)'")
+        return false
+    }
+
+}
+
+
+internal extension NSCalendar {
+
+    internal func isDateSameAsToday(date: NSDate) -> Bool {
+        if #available(iOS 8.0, iOSApplicationExtension 8.0, watchOS 2.0, *) {
+            return self.isDateInToday(date)
+        } else {
+            let today = NSDate()
+            let todayDay = self.ordinalityOfUnit(.Day, inUnit: .Year, forDate: today)
+            let todayYear = self.ordinalityOfUnit(.Year, inUnit: .Era, forDate: today)
+            let dateDay = self.ordinalityOfUnit(.Day, inUnit: .Year, forDate: date)
+            let dateYear = self.ordinalityOfUnit(.Year, inUnit: .Era, forDate: date)
+            return todayYear == dateYear && todayDay == dateDay
+        }
+    }
+
+}
