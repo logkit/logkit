9a99431ad32cb852a6f1256f5ec320d912bab2ed
justinpawela <justinpawela@users.noreply.github.com>
2016-03-31 20:10:04 -0500

    Update copyright notices in sources.

--- Source/Endpoints.swift
+++ Source/Endpoints.swift
@@ -1,6 +1,7 @@
 // Endpoints.swift
 //
-// Copyright (c) 2015, Justin Pawela & The LogKit Project (http://www.logkit.info/)
+// Copyright (c) 2015 - 2016, Justin Pawela & The LogKit Project
+// http://www.logkit.info/
 //
 // Permission to use, copy, modify, and/or distribute this software for any
 // purpose with or without fee is hereby granted, provided that the above
