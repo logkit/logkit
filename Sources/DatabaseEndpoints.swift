489fef6f767eaf76eda11f872209bd599cf3bdcd
Junho Shin <>
2019-08-22 12:37:33 -0600

    * changing DB endpoint name to be consistence

--- Sources/DatabaseEndpoints.swift
+++ Sources/DatabaseEndpoints.swift
@@ -18,63 +18,45 @@
 import Foundation
 import CoreData
 
-public class dbEndpoint{
+public class LXDataBaseEndpoint{
     
     lazy var persistentContainer: NSPersistentContainer = {
         let messageKitBundle = Bundle(identifier: "info.logkit.LogKit")
         let modelURL = messageKitBundle!.url(forResource: "LogKit", withExtension: "momd")
         let managedObjectModel = NSManagedObjectModel(contentsOf: modelURL!)
-        
-        
         let container = NSPersistentContainer(name: "LogKit", managedObjectModel: managedObjectModel!)
-        
-        
+ 
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
+                //Replace fatalError
                 fatalError("Unresolved error \(error), \(error.userInfo)")
             }
         })
         return container
     }()
     
-    // MARK: - Core Data Saving support
     func saveContext () {
         let context = persistentContainer.viewContext
         if context.hasChanges {
             do {
                 try context.save()
             } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
                 let nserror = error as NSError
+                //Replace fatalError
                 fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
             }
         }
     }
     
     func createData(){
-
         let managedContext = persistentContainer.viewContext
-        
         let logEntity = NSEntityDescription.entity(forEntityName: "Logs", in: managedContext)!
-        
         let user = NSManagedObject(entity: logEntity, insertInto: managedContext)
         let currentTime = round(NSDate().timeIntervalSince1970 * 1000)
         user.setValue(currentTime, forKey: "timeStamp")
         user.setValue("TESTING CORE DATA", forKey: "message")
         user.setValue(false, forKey: "sent")
-        
+
         do {
             try managedContext.save()
             
